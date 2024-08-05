de5f57a83ab00f6b50d5e19dd056b4ffc6eeeba7
cdavis12667 <cdavis13889@gmail.com>
2024-08-05 10:44:57 -0500

    Added classes for Achievements, Game, and Notifications.

---
+++ app/src/main/java/com/scottparrillo/gamepulse/Achievement.java
@@ -0,0 +1,9 @@
+package com.scottparrillo.gamepulse;
+
+public class Achievement {
+    boolean achievEarned = false;
+    String achievName = "";
+    String achievDescription = "";
+    //This should be the percentage of users that have earned this achievement
+    float publicPercentEarned = 0;
+}
