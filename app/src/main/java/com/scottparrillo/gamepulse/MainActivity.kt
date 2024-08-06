ffa0b82266005b0790fc3b227545eca7f6d8d4bd
cdavis12667 <cdavis13889@gmail.com>
2024-08-06 12:22:18 -0500

    Used compose to set up the skeleton of the library screen. Just need to fill in the bits and set events

--- app/src/main/java/com/scottparrillo/gamepulse/MainActivity.kt
+++ app/src/main/java/com/scottparrillo/gamepulse/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.scottparrillo.gamepulse
 
 import android.os.Bundle
+import android.view.RoundedCorner
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
@@ -14,11 +16,17 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.AbsoluteRoundedCornerShape
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Notifications
 import androidx.compose.material.icons.filled.Settings
@@ -29,10 +37,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+
 import com.scottparrillo.gamepulse.ui.theme.GamePulseTheme
 
 class MainActivity : ComponentActivity() {
@@ -185,4 +195,115 @@ fun DefaultPreview() {
     GamePulseTheme {
         HomeScreen()
     }
+}
+//This is the Start of the Library Screen
+@Preview(showBackground = true)
+@Composable
+fun LibraryScreen(){
+    //Main column
+    Column (modifier = Modifier.fillMaxSize()){
+        //Title text
+        Row{
+            Text(
+                text = "Library Screen",
+                style = MaterialTheme.typography.headlineLarge,
+                modifier = Modifier.padding(vertical = 16.dp)
+                )
+            }
+        //This contains the game catagories
+        LazyRow (
+            horizontalArrangement = Arrangement.spacedBy(32.dp),
+            modifier = Modifier
+                .background(Color.Transparent)
+                ){
+
+            item{
+                Box (
+                    modifier = Modifier
+                        .width(120.dp)
+                        .height(40.dp)
+                        .background(MaterialTheme.colorScheme.primaryContainer)
+                        .clickable { /* TODO: Handle Category clicks */ }
+                        .padding(8.dp)
+                        .clip(RoundedCornerShape(30.dp))
+                ){
+                    Text(text = "Recent")
+
+                }
+            }
+            item{
+                Box (
+                    modifier = Modifier
+                        .width(120.dp)
+                        .height(40.dp)
+                        .background(MaterialTheme.colorScheme.primaryContainer)
+                        .clickable { /* TODO: Handle Category clicks */ }
+                        .padding(8.dp)
+                        .clip(RoundedCornerShape(30.dp))
+                ){
+                    Text(text = "Current")
+
+                }
+            }
+            item{
+                Box (
+                    modifier = Modifier
+                        .width(120.dp)
+                        .height(40.dp)
+                        .background(MaterialTheme.colorScheme.primaryContainer)
+                        .clickable { /* TODO: Handle Category clicks */ }
+                        .padding(8.dp)
+                        .clip(RoundedCornerShape(30.dp))
+                ){
+                    Text(text = "Beaten")
+
+                }
+            }
+            item{
+                Box (
+                    modifier = Modifier
+                        .width(120.dp)
+                        .height(40.dp)
+                        .background(MaterialTheme.colorScheme.primaryContainer)
+                        .clickable { /* TODO: Handle Category clicks */ }
+                        .padding(8.dp)
+                        .clip(RoundedCornerShape(30.dp))
+
+                ){
+
+                    Text(text = "New", )
+
+                }
+            }
+
+        }
+        //This row should contain the sort icon
+        Row {
+            Image(painter = painterResource(id = R.drawable.profile_picture_placeholder),
+                contentDescription = "Profile Picture",)
+        }
+        LazyVerticalGrid(columns = GridCells.Adaptive(minSize = 128.dp)) {
+            //Replace these with images and two text blocks below them
+            item { Text(text = "test") }
+            item { Text(text = "test") }
+            item { Text(text = "test") }
+            item { Text(text = "test") }
+            item { Text(text = "test") }
+            item { Text(text = "test") }
+            item { Text(text = "test") }
+            item { Text(text = "test") }
+            item { Text(text = "test") }
+            item { Text(text = "test") }
+            item { Text(text = "test") }
+            item { Text(text = "test") }
+            item { Text(text = "test") }
+            item { Text(text = "test") }
+            item { Text(text = "test") }
+            item { Text(text = "test") }
+            item { Text(text = "test") }
+            item { Text(text = "test") }
+        }
+
+    }
+
 }
