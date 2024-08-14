750bbd0aa3eb77a0362f510e26518bcfd954d1b9
Bluefirepiro <Bluefirepiro@gmail.com>
2024-08-14 18:04:17 -0400

    added friends screen, and other functionality to app

---
+++ app/src/main/java/com/scottparrillo/gamepulse/FriendsAdapter.kt
@@ -0,0 +1,25 @@
+package com.scottparrillo.gamepulse
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class FriendsAdapter(private val friends: List<String>) : RecyclerView.Adapter<FriendsAdapter.FriendViewHolder>() {
+
+    class FriendViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val friendName: TextView = view.findViewById(R.id.friendName)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FriendViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_friend, parent, false)
+        return FriendViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: FriendViewHolder, position: Int) {
+        holder.friendName.text = friends[position]
+    }
+
+    override fun getItemCount() = friends.size
+}
