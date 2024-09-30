a03961bfd1afec6269bf43d210c5fa5fecb523a5
cdavis12667 <cdavis13889@gmail.com>
2024-09-30 07:02:34 -0500

    1. Api call to get achivements is now using asynch but wait until the call to get games is done.

--- app/src/main/java/com/scottparrillo/gamepulse/GameImportActivity.kt
+++ app/src/main/java/com/scottparrillo/gamepulse/GameImportActivity.kt
@@ -5,6 +5,7 @@ import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.compose.setContent
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
@@ -38,9 +39,13 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.scottparrillo.gamepulse.com.scottparrillo.gamepulse.SteamPlayerAchievements
 import com.scottparrillo.gamepulse.ui.theme.CuriousBlue
 import com.scottparrillo.gamepulse.ui.theme.GamePulseTheme
 import com.scottparrillo.gamepulse.ui.theme.SpringGreen
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 import java.io.IOException
 import java.io.ObjectOutputStream
 import java.time.Instant
@@ -161,36 +166,73 @@ class GameImportActivity: AppCompatActivity() {
                                             gameconvert.newlyAdded = true
                                         }
                                         Game.gameList.add(gameconvert)
-
                                     }
                                     saveGameFile(Game.gameList)
                                 }
                                 //End of game import api call
                                 for (game in Game.gameList) {
-                                    //First call being made for getting the achievement
-                                    val achievementCall =
-                                        SteamRetrofit.apiSteam.apiS.getAllGameAchievements(
-                                            game.gameId, "4A7BFC2A3443A093EA9953FD5529C795",
-                                            steamId.toLong())
-                                    val achResponse = achievementCall.execute()
-                                    if (achResponse.isSuccessful) {
-                                        val achievementPost = achResponse.body()!!
-                                        val playerStats = achievementPost.playerstats!!
-                                        val gameAchievements = playerStats.achievements
-                                        for (ach in gameAchievements) {
-                                            val earnedFlag =
-                                                //Ignore the warning
-                                                if (ach.achieved == 1) true else false
-                                            val toConvert = Achievement(0, ach.apiname,
+                                   val callAch = SteamRetrofit.apiSteam.apiS.getAllGameAchievements(
+                                        game.gameId, "4A7BFC2A3443A093EA9953FD5529C795",
+                                        steamId.toLong())
+                                    callAch.enqueue(object: Callback<SteamPlayerAchievements> {
+                                        override fun onResponse(
+                                            p0: Call<SteamPlayerAchievements>,
+                                            achResponse: Response<SteamPlayerAchievements>
+                                        ) {
+                                            if(achResponse.isSuccessful){
+                                                val achievementPost = achResponse.body()!!
+                                                val playerStats = achievementPost.playerstats!!
+                                                val gameAchievements = playerStats.achievements
+                                                for (ach in gameAchievements) {
+                                                    val earnedFlag =
+                                                        //Ignore the warning
+                                                        if (ach.achieved == 1) true else false
+                                                    val toConvert = Achievement(0, ach.apiname,
+                                                        "", 0.0,
+                                                        earnedFlag, 0, 0, 0)
+                                                    game.achievements.add(toConvert)
+                                                }
+                                            }
+                                        }
+
+                                        override fun onFailure(
+                                            p0: Call<SteamPlayerAchievements>,
+                                            p1: Throwable
+                                        ) {
+                                            p1.printStackTrace()
+                                        }
+                                    })
+                                    /*
+                                        //First call being made for getting the achievement
+                                        val achievementCall =
+                                            SteamRetrofit.apiSteam.apiS.getAllGameAchievements(
+                                                game.gameId, "4A7BFC2A3443A093EA9953FD5529C795",
+                                                steamId.toLong())
+                                        val achResponse = achievementCall.execute()
+                                        if (achResponse.isSuccessful) {
+                                            val achievementPost = achResponse.body()!!
+                                            val playerStats = achievementPost.playerstats!!
+                                            val gameAchievements = playerStats.achievements
+                                            for (ach in gameAchievements) {
+                                                val earnedFlag =
+                                                    //Ignore the warning
+                                                    if (ach.achieved == 1) true else false
+                                                val toConvert = Achievement(0, ach.apiname,
                                                     "", 0.0,
                                                     earnedFlag, 0, 0, 0)
-                                                    game.achievements.add(toConvert)
+                                                game.achievements.add(toConvert)
+                                            }
                                         }
+
                                     }
+
+                                     */
+
                                 }
                                 saveGameFile(Game.gameList)
-                                steamIdText = "Done Importing"
+
                                 }
+                            steamIdText = "Done Importing"
 
                         }, modifier = Modifier.padding(horizontal = 0.dp),
                         colors = ButtonDefaults.buttonColors(containerColor = SpringGreen)
